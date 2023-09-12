f8631c316702a95704f96de64e1f986fddcafd5f
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-09-12 02:15:50 -0300

    fim

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,17 @@ let validator = {
 
         let inputs = form.querySelectorAll('input');
 
+        validator.clearErrors()
+
         for(let i = 0;i<inputs.length;i++){
             let input = inputs[i]
             let check = validator.checkInput(input);
             if(check !== true){
                 send = false;
+               validator.showError(input, check)
                 //exibir erro
             }
         }
-        send = false
 
         if(send){
             form.submit()
@@ -33,16 +35,47 @@ let validator = {
                         }
                     break;
                     case 'min':
+                        if(input.value.length < rDtails[1]){
+                            return 'Campo tem que ter pelo menos '+rDtails[1]+ ' caracteres'
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != ''){
+                            let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                            if(!regex.test(input.value.toLowerCase())){
+                                return 'e-mail digitado não é valido'
+                            }
 
-                    break
+                        }
                 }
             }
         }
 
         return true
 
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000'
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+        
+        input.parentElement.insertBefore(errorElement, input.ElementSibiling)
+
+    },
+    clearErrors:()=>{
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++){
+            inputs[i].style = ''
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+        for(let i =0;i<errorElements.length;i++){
+            errorElements[i].remove();
+        }
     }
-}
+};
 
 let form = document.querySelector('.validator');
 form.addEventListener('submit', validator.handleSubmit);
